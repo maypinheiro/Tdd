8815d12cdac246d5145c0ad9f18ce422d2801a03
maypinheiro <mayarapinheirodesousa@hotmail.com>
2023-02-08 18:46:58 -0300

    Adicionar arquivos de projeto.

---
+++ NerdStore.Vendas.Application.Tests/PedidoTest.cs
@@ -0,0 +1,22 @@
+using System;
+using Xunit;
+
+namespace NerdStore.Vendas.Application.Tests
+{
+    public class PedidoTest
+    {
+        [Fact(DisplayName = "Adicionar Item Novo Pedido")]
+        [Trait("Categoria","Vendas -Pedido")]
+        public void AdicionarItemPedido_NovoPedido_DeveAtualizarValor()
+        {
+            //Arrange
+            var pedido = new Pedido();
+            var pedidoItem = new PedidoItem(Guid.NewGuid(),"ProdutoTest",2,100);
+            // Act
+            pedido.AdicionarItem(pedidoItem);
+            //Assert
+            Assert.Equal(200, pedido.ValorTotal);
+
+        }
+    }
+}
---
+++ NerdStore/Pedido.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace NerdStore
+{
+    public class Pedido
+    {
+        public Pedido()
+        {
+            _pedidoItens = new List<PedidoItem>();
+        }
+        public decimal ValorTotal { get; private set; }
+        private readonly List<PedidoItem> _pedidoItens;
+        public IReadOnlyCollection<PedidoItem> PedidoItens => _pedidoItens;
+
+        public void AdicionarItem(PedidoItem pedidoItem) {
+            _pedidoItens.Add(pedidoItem);
+            ValorTotal = PedidoItens.Sum(i=> i.Quantidade * i.ValorUnitario);
+        }
+    }
+    public class PedidoItem
+    {
+        public PedidoItem(Guid produtoId, string produtoNome, int quantidade, decimal valorUnitario)
+        {
+            ProdutoId = produtoId;
+            ProdutoNome = produtoNome;
+            Quantidade = quantidade;
+            ValorUnitario = valorUnitario;
+        }
+
+        public Guid ProdutoId { get; private set; }
+        public string ProdutoNome { get; private set; }
+        public int Quantidade { get; private set; }
+        public decimal ValorUnitario { get; private set; }
+
+ 
+    }
+}
