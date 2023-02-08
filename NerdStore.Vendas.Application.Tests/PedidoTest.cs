0514de6fb097abdb094263a21b07ad3721040804
maypinheiro <mayarapinheirodesousa@hotmail.com>
2023-02-08 20:08:16 -0300

    Item 1.2 do roteiro

--- NerdStore.Vendas.Application.Tests/PedidoTest.cs
+++ NerdStore.Vendas.Application.Tests/PedidoTest.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using Xunit;
 
 namespace NerdStore.Vendas.Application.Tests
@@ -10,12 +11,33 @@ namespace NerdStore.Vendas.Application.Tests
         public void AdicionarItemPedido_NovoPedido_DeveAtualizarValor()
         {
             //Arrange
-            var pedido = new Pedido();
+            var pedido = Pedido.PedidoFactory.NovoPedidoRascunho(Guid.NewGuid());
             var pedidoItem = new PedidoItem(Guid.NewGuid(),"ProdutoTest",2,100);
             // Act
             pedido.AdicionarItem(pedidoItem);
             //Assert
             Assert.Equal(200, pedido.ValorTotal);
         }
+
+
+        [Fact(DisplayName = "Adicionar Item Pedido Existente")]
+        [Trait("Categoria", "Vendas -Pedido")]
+        public void AdicionarItemPedido_ItemExistente_DeveIncrementarUnidadesSomarValores()
+        {
+            //Arrange
+            var pedido = Pedido.PedidoFactory.NovoPedidoRascunho(Guid.NewGuid());
+            var produtoId = Guid.NewGuid();
+            var pedidoItem = new PedidoItem(produtoId, "ProdutoTest", 2, 100);
+            pedido.AdicionarItem(pedidoItem);
+            var pedidoItem2= new PedidoItem(produtoId, "ProdutoTest", 1, 100);
+
+            //Act
+            pedido.AdicionarItem(pedidoItem2);
+            //Assert
+            Assert.Equal(300, pedido.ValorTotal);
+            Assert.Equal(1, pedido.PedidoItens.Count);
+            Assert.Equal(3, pedido.PedidoItens.FirstOrDefault(p=> p.ProdutoId == produtoId).Quantidade);
+        }
+
     }
 }
--- NerdStore/Pedido.cs
+++ NerdStore/Pedido.cs
@@ -7,20 +7,73 @@ namespace NerdStore
 {
     public class Pedido
     {
-        public Pedido()
+        protected Pedido()
         {
             _pedidoItens = new List<PedidoItem>();
         }
 
+        public Guid ClienteId { get; private set; }
         public decimal ValorTotal { get; private set; }
+        public PedidoStatus PedidoStatus { get; private set; }
         private readonly List<PedidoItem> _pedidoItens;
         public IReadOnlyCollection<PedidoItem> PedidoItens => _pedidoItens;
 
+        public void CalcularValorPedido()
+        {
+            ValorTotal = PedidoItens.Sum(i => i.CalcularValor());
+        }
+
         public void AdicionarItem(PedidoItem pedidoItem) {
+
+            if (_pedidoItens.Any(p=> p.ProdutoId == pedidoItem.ProdutoId))
+            {
+                var itemExistente = _pedidoItens.FirstOrDefault(p => p.ProdutoId == pedidoItem.ProdutoId);
+                itemExistente.AdicionarUnidades(pedidoItem.Quantidade);
+                pedidoItem = itemExistente;
+
+                _pedidoItens.Remove(itemExistente);
+            }
+
             _pedidoItens.Add(pedidoItem);
-            ValorTotal = PedidoItens.Sum(i=> i.Quantidade * i.ValorUnitario);
+            CalcularValorPedido();
+
+        }
+
+        public void TornarRascunho()
+        {
+            PedidoStatus = PedidoStatus.Rascunho;
+        }
+
+        public static class PedidoFactory
+        {
+            public static Pedido NovoPedidoRascunho(Guid clienteId)
+            {
+                var pedido = new Pedido
+                {
+                    ClienteId = clienteId,
+                };
+
+                pedido.TornarRascunho();
+                return pedido;
+            }
         }
     }
+
+
+
+
+
+    public enum PedidoStatus
+    {
+        Rascunho =0,
+        Iniciado =1,
+        Pago =4,
+        Entregue =5,
+        Cancelado =6
+
+    }
+
+
     public class PedidoItem
     {
         public PedidoItem(Guid produtoId, string produtoNome, int quantidade, decimal valorUnitario)
@@ -35,6 +88,17 @@ namespace NerdStore
         public string ProdutoNome { get; private set; }
         public int Quantidade { get; private set; }
         public decimal ValorUnitario { get; private set; }
+
+        internal void AdicionarUnidades(int unidades)
+        {
+            Quantidade += unidades;
+        }
+
+        internal decimal CalcularValor()
+        {
+            return Quantidade * ValorUnitario;
+        }
+ 
 
  
     }
